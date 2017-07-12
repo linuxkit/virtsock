870c75afdd0c3472fc7c5a906598abf1741469a7
Rolf Neugebauer <rolf.neugebauer@docker.com>
2017-07-12 23:42:07 +0100

    sock_stress: Move echo code into a separate file
    This code is a specific test of an echo server over a
    SOCK_STREAM socket. Subsequent commits will add different
    tests.
    
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

--- cmd/sock_stress/main.go
+++ cmd/sock_stress/main.go
@@ -3,18 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"log"
+	"math/rand"
 	"net"
 	"net/url"
 	"os"
 	"path/filepath"
 	"sync"
-	"time"
-
-	"crypto/md5"
-	"math/rand"
 	"sync/atomic"
+	"time"
 
 	"github.com/linuxkit/virtsock/pkg/hvsock"
 )
@@ -172,45 +169,6 @@ func parseSockStr(inStr string) Sock {
 	return nil
 }
 
-func server(s Sock) {
-	l := s.Listen()
-	defer l.Close()
-
-	connid := 0
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatalf("Accept(): %s\n", err)
-		}
-
-		prDebug("[%05d] accept(): %s -> %s \n", connid, conn.RemoteAddr(), conn.LocalAddr())
-		go handleRequest(conn, connid)
-		connid++
-	}
-}
-
-func handleRequest(c net.Conn, connid int) {
-	defer func() {
-		prDebug("[%05d] Closing\n", connid)
-		err := c.Close()
-		if err != nil {
-			prError("[%05d] Close(): %s\n", connid, err)
-		}
-	}()
-
-	start := time.Now()
-
-	n, err := io.Copy(c, c)
-	if err != nil {
-		prError("[%05d] Copy(): %s", connid, err)
-		return
-	}
-
-	diffTime := time.Since(start)
-	prInfo("[%05d] ECHOED: %10d bytes in %10.4f ms\n", connid, n, diffTime.Seconds()*1000)
-}
-
 func parClient(wg *sync.WaitGroup, s Sock) {
 	connid := int(atomic.AddInt32(&connCounter, 1))
 	for connid < connections {
@@ -220,135 +178,4 @@ func parClient(wg *sync.WaitGroup, s Sock) {
 	}
 
 	wg.Done()
-}
-
-func client(s Sock, conid int) {
-	c, err := s.Dial(conid)
-	if err != nil {
-		prError("[%05d] Failed to Dial: %s %s\n", conid, s, err)
-		return
-	}
-	defer c.Close()
-
-	// Create buffer with random data and random length.
-	// Make sure the buffer is not zero-length
-	buflen := minDataLen
-	if maxDataLen > minDataLen {
-		buflen += rand.Intn(maxDataLen - minDataLen + 1)
-	}
-	hash0 := md5.New()
-
-	var txTime, rxTime time.Duration
-	start := time.Now()
-
-	w := make(chan int)
-	go func() {
-		total := 0
-		remaining := buflen
-		for remaining > 0 {
-			batch := 0
-			bufsize := minBufLen
-			if maxBufLen > minBufLen {
-				bufsize += rand.Intn(maxBufLen - minBufLen + 1)
-			}
-			if remaining > bufsize {
-				batch = bufsize
-			} else {
-				batch = remaining
-			}
-
-			txbuf := randBuf(batch)
-			hash0.Write(txbuf)
-
-			e := make(chan error, 0)
-			go func() {
-				l, err := c.Write(txbuf)
-				if err != nil {
-					e <- err
-				} else if l != batch {
-					e <- fmt.Errorf("Sent incorrect length: expected %d, got %d", batch, l)
-				} else {
-					e <- nil
-				}
-			}()
-
-			select {
-			case err := <-e:
-				if err != nil {
-					prError("[%05d] Failed to send: %s\n", conid, err)
-					break
-				}
-			case <-time.After(ioTimeout):
-				prError("[%05d] Send timed out\n", conid)
-				break
-			}
-
-			total += batch
-			remaining -= batch
-		}
-
-		// Tell the other end that we are done
-		c.CloseWrite()
-
-		txTime = time.Since(start)
-		w <- total
-	}()
-
-	hash1 := md5.New()
-
-	totalReceived := 0
-	remaining := buflen
-	for remaining > 0 {
-		batch := 0
-		bufsize := minBufLen
-		if maxBufLen > minBufLen {
-			bufsize += rand.Intn(maxBufLen - minBufLen + 1)
-		}
-		if remaining > bufsize {
-			batch = bufsize
-		} else {
-			batch = remaining
-		}
-
-		rxbuf := make([]byte, batch)
-
-		e := make(chan error, 0)
-		go func() {
-			l, err := io.ReadFull(c, rxbuf)
-			if err != nil {
-				e <- err
-			} else if l != batch {
-				e <- fmt.Errorf("Received incorrect length, expected %d, got %d", batch, l)
-			} else {
-				e <- nil
-			}
-		}()
-
-		select {
-		case err := <-e:
-			if err != nil {
-				prError("[%05d] Failed to receive: %s\n", conid, err)
-				break
-			}
-		case <-time.After(ioTimeout):
-			prError("[%05d] Receive timed out\n", conid)
-			break
-		}
-
-		hash1.Write(rxbuf)
-		remaining -= batch
-		totalReceived += batch
-	}
-
-	rxTime = time.Since(start)
-	totalSent := <-w
-
-	csum0 := md5Hash(hash0)
-	prDebug("[%05d] TX: %d bytes, md5=%02x in %s\n", conid, totalSent, csum0, txTime)
-
-	csum1 := md5Hash(hash1)
-	prInfo("[%05d] TX/RX: %10d bytes in %10.4f ms\n", conid, totalReceived, rxTime.Seconds()*1000)
-	if csum0 != csum1 {
-		prError("[%05d] Checksums don't match", conid)
-	}
 }
